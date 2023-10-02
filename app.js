a2f59e5f6891fb00c18ba4e7d1cbd1695decb84b
DarioValer <dario-valeriano@hotmail.com>
2023-10-02 17:35:16 -0300

    Puerto

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.use(express.static('public'));
 
 
 app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Servidor funcionando puerto 5055');
+    console.log('Servidor funcionando puerto 3000');
 });
 
 app.get('/', (req,res)=>{
